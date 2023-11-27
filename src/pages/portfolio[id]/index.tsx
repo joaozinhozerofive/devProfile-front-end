92c53b8c069a5c4693ab2f13e6d50b56fd256118
joão <joaovitormachadororato@gmail.com>
2023-11-27 10:32:34 -0300

    save filed dynamic routes

---
+++ src/pages/portfolio[id]/index.tsx
@@ -0,0 +1,7 @@
+import styles from './styles.module.scss'
+
+export default function Portfolio(){
+    return (
+        <h2 className={styles.teste}>Você está na pagina de portifolio</h2>
+    )
+}
