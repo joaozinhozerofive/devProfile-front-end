c1436aeaf57fd852c6a19005afe411fb952f26fe
joão <joaovitormachadororato@gmail.com>
2023-12-12 18:36:44 -0300

    debug tests

--- src/pages/portfolio/[id]/index.tsx
+++ src/pages/portfolio/[id]/index.tsx
@@ -58,6 +58,9 @@ export default function Portfolio({id} :  PorfolioProps ){
                     console.log(error)
                 
             }
+
+
+            console.log(id)
    
            
         }
